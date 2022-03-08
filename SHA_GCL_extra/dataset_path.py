9638a43f66b79d632193bb2be3234e0340c72071
dongxingning <58069157+dongxingning@users.noreply.github.com>
2022-03-08 15:18:26 +0800

    Update dataset_path.py

--- SHA_GCL_extra/dataset_path.py
+++ SHA_GCL_extra/dataset_path.py
@@ -1,13 +1,1 @@
-choice = 'dxn'
-
-if choice == 'dxn':
-    datasets_path = '/home/share/dongxingning/'
-elif choice == 'gjz':
-    datasets_path = '/home/share/guojiazhen/borrowed/'
-elif choice == 'cyh':
-    datasets_path = '/data/caiyuhang/dl/'
-elif choice == '221':
-    datasets_path = '/data/renjixietong/'
-else:
-    datasets_path = 'G:/all_dataset/'
-
+datasets_path = '/home/share/yourpath/'
